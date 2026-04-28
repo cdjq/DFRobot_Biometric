84d1ec01e475434d1118603a8403639eaa64c5d6
cdjq <2803044540@qq.com>
2026-04-28 13:31:42 +0800

    第一次提交

---
+++ DFRobot_Biometric.cpp
@@ -0,0 +1,292 @@
+/*!
+ * @file DFRobot_Sensor.cpp
+ * @brief 定义DFRobot_Sensor 类的基础结构，基础方法的实现
+ * @copyright	Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license The MIT License (MIT)
+ * @author [Ouki](ouki.wang@dfrobot.com)
+ * @version V1.0
+ * @date 2019-07-13
+ * @url https://github.com/DFRobot/DFRobot_Sensor
+ */
+#include "DFRobot_Biometric.h"
+
+#include <Arduino.h>
+
+DFRobot_Biometric::DFRobot_Biometric(Stream& serial) : _serial(serial)
+{
+  _serial = serial;
+}
+
+bool DFRobot_Biometric::begin(void)
+{
+  uint8_t  data[6]     = { 0xEF, 0xAA, 0x11, 0x00, 0x00, 0x11 };
+  bool     state       = false;
+  uint16_t bufLen      = 0;
+  uint8_t  buffer[250] = { 0 };
+  calculateChecksum(data, 6);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 6);
+  state = waitForReply(buffer, bufLen, 1000);
+  if (state == true) {
+    if (buffer[7] == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void DFRobot_Biometric::calculateChecksum(uint8_t* data, uint8_t len)
+{
+  uint8_t checkSum = 0;
+  for (uint8_t i = 2; i < len - 1; i++) {
+    checkSum ^= data[i];
+  }
+  data[len - 1] = checkSum;
+}
+
+int16_t DFRobot_Biometric::getAllNumsFaceUserIDs(void)
+{
+
+  uint8_t  data[7]     = { 0xEF, 0xAA, 0x24, 0x00, 0x01, 0x01, 0x00 };
+  bool     state       = false;
+  uint16_t bufLen      = 0;
+  int16_t  nums        = 0;
+  uint8_t  buffer[250] = { 0 };
+  calculateChecksum(data, 7);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 7);
+  state = waitForReply(buffer, bufLen, 1000);
+  if (state == true) {
+    nums = buffer[7] * 256 + buffer[8];
+    return nums;
+  }
+  while (_serial.available()) {
+    _serial.read();
+  }
+  return -1;
+}
+
+int16_t DFRobot_Biometric::getAllNumsPalmUserIDs(void)
+{
+  uint8_t  data[7]     = { 0xEF, 0xAA, 0x24, 0x00, 0x01, 0x02, 0x00 };
+  bool     state       = false;
+  uint16_t bufLen      = 0;
+  int16_t  nums        = 0;
+  uint8_t  buffer[250] = { 0 };
+  calculateChecksum(data, 7);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 7);
+  state = waitForReply(buffer, bufLen, 1000);
+  if (state == true) {
+    nums = buffer[7] * 256 + buffer[8];
+    return nums;
+  }
+  while (_serial.available()) {
+    _serial.read();
+  }
+  return -1;
+}
+
+bool DFRobot_Biometric::waitForReply(uint8_t* buffer, uint16_t& bufLen, uint16_t timeout)
+{
+  uint16_t recmdLen  = 0;
+  uint8_t  recData   = 0;
+  uint32_t startTime = millis();
+  while (millis() - startTime < timeout) {
+    if (_serial.available()) {
+      recData = _serial.read();
+      if (bufLen < 3) {
+        if (recData == 0xEF && bufLen == 0) {
+          buffer[bufLen++] = recData;
+        } else if (recData == 0xAA && bufLen == 1) {
+          buffer[bufLen++] = recData;
+        } else if (recData == 0x00 && bufLen == 2) {
+          buffer[bufLen++] = recData;
+        } else if (recData == 0x01 && bufLen == 2) {
+          bufLen = 0;
+        } else {
+          bufLen = 0;
+        }
+      } else {
+        if (bufLen == 3) {
+          buffer[bufLen++] = recData;
+        } else if (bufLen == 4) {
+          buffer[bufLen++] = recData;
+          recmdLen         = buffer[3] * 256 + buffer[4];
+        } else if (bufLen > 4 && bufLen <= (4 + recmdLen)) {
+          buffer[bufLen++] = recData;
+        } else if (bufLen == (5 + recmdLen)) {
+          calculateChecksum(buffer, bufLen + 1);
+          if (recData == buffer[bufLen]) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
+uint8_t DFRobot_Biometric::enrollUser(uint8_t kind)
+{
+  uint8_t  data[46] = { 0xEF, 0xAA, 0x26, 0x00, 0x28, 0x00 };
+  uint16_t i = 0, bufLen = 0;
+  bool     state       = false;
+  uint8_t  buffer[250] = { 0 };
+  for (i = 6; i < 6 + 32; i++) {
+    data[i] = 0x00;
+  }
+  if (kind == 1) {
+    data[38] = 0x00;
+  } else if (kind == 2) {
+    data[38] = 0xFD;
+  }
+  data[39] = 0x01;    //单帧录入
+  data[40] = 0x00;    //0为人脸不可重复录入，但用户名可重复录入;1为可重复录入
+  data[41] = 0x0A;    //等待时间
+  data[42] = 0x00;
+  data[43] = 0x00;
+  data[44] = 0x00;
+  calculateChecksum(data, 46);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 46);
+  state = waitForReply(buffer, bufLen, 11000);
+  if (state == true) {
+    if (buffer[6] == 0x00) {
+      return 1;
+    } else if (buffer[6] == 0x0A) {
+      return 2;
+    } else if (buffer[6] == 0x0D) {
+      return 3;
+    }
+  }
+  while (_serial.available()) {
+    _serial.read();
+  }
+  return 0;
+}
+
+uint8_t DFRobot_Biometric::deleteUser(uint16_t id)    //能够成功删除指定用户，但删除不存在的用户不会报错
+{
+
+  uint8_t  data[9]     = { 0xEF, 0xAA };
+  uint8_t  buffer[250] = { 0 };
+  uint16_t bufLen      = 0;
+  bool     state       = false;
+  if (id <= 500) {
+    data[2] = 0x20;    //人脸指令
+    data[7] = 0x00;    //人脸
+  } else if (id > 500) {
+    data[2] = 0x80;    //掌静脉指令
+    data[7] = 0x01;    //掌静脉
+  }
+  data[3] = 0x00;
+  data[4] = 0x03;
+  data[5] = id / 256;
+  data[6] = id % 256;
+  calculateChecksum(data, 9);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 9);
+  state = waitForReply(buffer, bufLen, 11000);
+  if (state == true) {
+    if (buffer[6] == 0x00) {
+      return 1;
+    } else if (buffer[6] == 0x08) {
+      return 2;
+    }
+  }
+  while (_serial.available()) {
+    _serial.read();
+  }
+  return 0;
+}
+
+uint8_t DFRobot_Biometric::deleteAllUser(void)    //??选择范围删除时，人脸和掌静脉都被删除了？删除后得不到正确的指令，但都能成功删除
+{
+  uint8_t  data[11]    = { 0xEF, 0xAA, 0x21, 0x00, 0x05 };
+  uint8_t  buffer[250] = { 0 };
+  uint16_t bufLen      = 0;
+  bool     state       = false;
+  data[5]              = 0x00;
+  data[6]              = 0x00;
+  data[7]              = 0x00;
+  data[8]              = 0x00;
+  data[9]              = 0x00;
+  calculateChecksum(data, 11);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 11);
+  state = waitForReply(buffer, bufLen, 5000);
+  if (state == true) {
+    if (buffer[6] == 0x00) {
+      return 1;
+    } else if (buffer[6] == 0x05) {
+      return 2;
+    }
+  }
+  while (_serial.available()) {
+    _serial.read();
+  }
+  return 0;
+}
+
+void DFRobot_Biometric::getRecognitionResult(sId_t* ID)
+{
+  uint8_t  data[8]     = { 0xEF, 0xAA, 0x12, 0x00, 0x02, 0x00, 0x10, 0x00 };
+  uint8_t  buffer[250] = { 0 };
+  uint16_t bufLen      = 0;
+  bool     state       = false;
+  calculateChecksum(data, 8);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 8);
+  state = waitForReply(buffer, bufLen, 5000);
+  if (state == true) {
+    if (buffer[6] == 0 && ((buffer[42] == 0xC8) || buffer[42] == 0xCC)) {
+      ID->id    = buffer[7] * 256 + buffer[8];
+      ID->admin = buffer[41];
+    } else if (buffer[6] == 0 && buffer[42] == 0xFA) {
+      ID->id    = buffer[7] * 256 + buffer[8];
+      ID->admin = buffer[41];
+    } else if (buffer[6] == 0x0D) {
+      ID->id = -1;
+    }
+  }
+  while (_serial.available()) {
+    _serial.read();
+  }
+}
+
+uint8_t DFRobot_Biometric::LEDcolor(uint8_t color, uint8_t kind)
+{
+  uint8_t  data[8]     = { 0xEF, 0xAA, 0x90, 0x00, 0x02, 0x00, 0x00, 0x00 };
+  uint8_t  buffer[250] = { 0 };
+  uint16_t bufLen      = 0;
+  bool     state       = false;
+  data[5]              = color;
+  data[6]              = kind;
+  calculateChecksum(data, 8);
+  while (_serial.available()) {
+    _serial.read();
+  }
+  _serial.write(data, 8);
+  state = waitForReply(buffer, bufLen, 1500);
+  if (state == true) {
+    if (buffer[6] == 0x00) {
+      return 1;
+    }
+  }
+  return 0;
+}
---
+++ DFRobot_Biometric.h
@@ -0,0 +1,151 @@
+/*!
+ * @file DFRobot_Biometric.h
+ * @brief 定义DFRobot_Biometric 类的基础结构
+ * @details 这是一个人脸与掌静脉识别模块，通过串口和USB去发送命令
+ * @n 向寄存器0里写入数据，点亮不同颜色的LED灯
+ * @n 从寄存器2里读出数据，高四位表示光线强度，低四位表示声音强度
+ * @n 从寄存器3 bit0里写入数据，写1表示正常模式，写0表示低功耗模式
+ * @n 从寄存器4 读取数据，读到的是芯片版本0xDF
+ * @copyright	Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license The MIT License (MIT)
+ * @author [Ouki](ouki.wang@dfrobot.com)
+ * @version V1.0
+ * @date 2026-04-27
+ * @url https://github.com/DFRobot/DFRobot_Biometric
+ */
+#ifndef __DFROBOT_BIOMETRIC_H
+#define __DFROBOT_BIOMETRIC_H
+#include <Arduino.h>
+class DFRobot_Biometric {
+public:
+  /**
+   * @struct sId_t
+   * @brief 用于存储识别函数返回的信息
+   * @details 提示一些必要的技术细节(从数据手册上抄写)
+   * @note 注解
+   * @attention 当id<0时，表示识别失败
+   */
+  typedef struct {
+    int16_t id;
+    uint8_t admin;
+  } sId_t;
+
+  /**
+   * @fn DFRobot_Biometric
+   * @brief 构造函数
+   * @details 函数细节描述(简单函数可以不需要)
+   * @param serial 定义对象时，传进来初始化的一个串口
+   * @return None
+   */
+  DFRobot_Biometric(Stream& serial);
+
+  /**
+   * @fn begin
+   * @brief 确认模块是否就绪,是否空闲
+   * @details 函数细节描述(简单函数可以不需要)
+   * @param None (无，可以不需要)
+   * @return bool类型
+   * @retval true 就绪
+   * @retval false 未就绪
+   * @note 初始化或则执行某项命令前可以用begin()函数检查状态
+   * @attention 注意事项(没有可不需要)
+   */
+  bool begin(void);
+
+  /**
+   * @fn enrollUser
+   * @brief 对人脸或掌静脉进行识别
+   * @details 函数细节描述(简单函数可以不需要)
+   * @param kind 0表示进行人脸识别，1表示进行掌静脉识别
+   * @return 返回任务执行结果
+   * @retval 0 未作出回应
+   * @retval 1 表示成功
+   * @retval 2 表
+   * @retval 3 表示录入超时
+   */
+  uint8_t enrollUser(uint8_t kind);
+
+  /**
+   * @fn getAllNumsFaceUserIDs
+   * @brief 获取人脸用户的数量
+   * @details 函数细节描述(简单函数可以不需要)
+   * @return 人脸用户的数量
+   * @retval -1表示获取失败
+   */
+  int16_t getAllNumsFaceUserIDs(void);
+
+  /**
+   * @fn getAllNumsPalmUserIDs
+   * @brief 获取掌静脉用户的数量
+   * @details 函数细节描述(简单函数可以不需要)
+   * @return 掌静脉用户的数量
+   * @retval -1表示获取失败
+   */
+  int16_t getAllNumsPalmUserIDs(void);
+
+  /**
+   * @fn getRecognitionResult
+   * @brief 对用户进行识别
+   * @details 函数细节描述(简单函数可以不需要)
+   * @return none
+   */
+  void getRecognitionResult(sId_t* ID);
+
+  /**
+   * @fn deleteUser
+   * @brief 删除指定用户
+   * @details 函数细节描述(简单函数可以不需要)
+   * @return 删除结果
+   * @retval 0 删除失败
+   * @retval 1 删除成功
+   * @retval 2 无指定用户
+   */
+  uint8_t deleteUser(uint16_t id);
+
+  /**
+   * @fn deleteAllUser
+   * @brief 删除所有用户
+   * @details 函数细节描述(简单函数可以不需要)
+   * @return 删除结果
+   * @retval 0 删除失败
+   * @retval 1 成功删除
+   * @retval 2 未知错误，建议再次删除
+   */
+  uint8_t deleteAllUser(void);
+
+  /**
+   * @fn LEDcolor
+   * @brief 控制led的状态
+   * @details 函数细节描述(简单函数可以不需要)
+   * @param color 灯的颜色，0 绿灯，1 红灯，2 白灯
+   * @param kind 0 开灯，1 关灯
+   * @return 执行结果
+   * @retval 0执行失败
+   * @retval 1 执行成功
+   */
+  uint8_t LEDcolor(uint8_t color, uint8_t kind);
+
+private:
+  Stream& _serial;
+
+  /**
+   * @fn calculateChecksum
+   * @brief 计算命令的校验值
+   * @details 函数细节描述(简单函数可以不需要)
+   * @param data 存放命令的数组首地址
+   * @param len 命令长度
+   */
+  void calculateChecksum(uint8_t* data, uint8_t len);
+
+  /**
+   * @fn waitForReply
+   * @brief 发送命令后，等待读取reply反馈指令
+   * @details 函数细节描述(简单函数可以不需要)
+   * @param buffer 存放命令的数组首地址
+   * @param bufLen 数组索引
+   * @param timeout 等待时间
+   */
+  bool waitForReply(uint8_t* buffer, uint16_t& bufLen, uint16_t timeout);
+};
+
+#endif
